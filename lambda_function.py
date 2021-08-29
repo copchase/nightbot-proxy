e8c7aaac29bfe6d9ba6085252e414f7f3aaea53b
copchase <lscnr.thc@gmail.com>
2021-08-29 15:48:13 -0700

    Add logging

--- lambda_function.py
+++ lambda_function.py
@@ -2,12 +2,15 @@ import os
 import boto3
 import json
 
+from logzero import logger
 
 def lambda_handler(event: dict, context) -> dict:
     env = os.environ.get("ENV", "stage")
     operation = event["queryStringParameters"].get("operation")
 
     if operation:
+        logger.info(f"incoming operation for {env}-{operation}")
+        logger.info(f"event: {event}")
         lambda_client = boto3.client("lambda")
         lambda_client.invoke(
             FunctionName=f"{env}-{operation}",
